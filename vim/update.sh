464ee58541d61bf01640f55f3e8522eb768c78dc
Backalor <backalor@backalor.com>
2013-02-20 14:22:30 +0900

    Added some plugins and new key bindings

---
+++ vim/update.sh
@@ -0,0 +1,136 @@
+#!/bin/bash -e
+#
+# Updates Vim plugins.
+#
+# Update everything (long):
+#
+#   ./update.sh
+#
+# Update just the things from Git:
+#
+#   ./update.sh repos
+#
+# Update just one plugin from the list of Git repos:
+#
+#   ./update.sh repos powerline
+#
+
+
+vimdir=$HOME/.vim
+bundledir=$vimdir/plugins
+tmp=/tmp/$LOGNAME-vim-update
+me=.vim/update.sh
+
+# I have an old server with outdated CA certs.
+if [ -n "$INSECURE" ]; then
+  curl='curl --insecure'
+  export GIT_SSL_NO_VERIFY=true
+else
+  curl='curl'
+fi
+
+# URLS --------------------------------------------------------------------
+
+# This is a list of all plugins which are available via Git repos. git:// URLs
+# don't work.
+repos=(
+  https://github.com/Lokaltog/vim-powerline.git
+  https://github.com/altercation/vim-colors-solarized.git
+  https://github.com/kien/ctrlp.vim.git
+  https://github.com/scrooloose/syntastic.git
+  https://github.com/tpope/vim-pathogen.git
+  )
+
+# Here's a list of everything else to download in the format
+# <destination>;<url>[;<filename>]
+other=(
+  )
+
+case "$1" in
+
+  # GIT -----------------------------------------------------------------
+  repos)
+    mkdir -p $bundledir
+    for url in ${repos[@]}; do
+      if [ -n "$2" ]; then
+        if ! (echo "$url" | grep "$2" &>/dev/null) ; then
+          continue
+        fi
+      fi
+      dest="$bundledir/$(basename $url | sed -e 's/\.git$//')"
+      rm -rf $dest
+      echo "Cloning $url into $dest"
+      git clone $url $dest
+      rm -rf $dest/.git
+    done
+    ;;
+
+  # TARBALLS AND SINGLE FILES -------------------------------------------
+  other)
+    set -x
+    mkdir -p $bundledir
+    rm -rf $tmp
+    mkdir $tmp
+    pushd $tmp
+
+    for pair in ${other[@]}; do
+      parts=($(echo $pair | tr ';' '\n'))
+      name=${parts[0]}
+      url=${parts[1]}
+      filename=${parts[2]}
+      dest=$bundledir/$name
+
+      rm -rf $dest
+
+      if echo $url | egrep '.zip$'; then
+        # Zip archives from VCS tend to have an annoying outer wrapper
+        # directory, so unpacking them into their own directory first makes it
+        # easy to remove the wrapper.
+        f=download.zip
+        $curl -L $url >$f
+        unzip $f -d $name
+        mkdir -p $dest
+        mv $name/*/* $dest
+        rm -rf $name $f
+
+      else
+        # Assume single files. Create the destination directory and download
+        # the file there.
+        mkdir -p $dest
+        pushd $dest
+        if [ -n "$filename" ]; then
+          $curl -L $url >$filename
+        else
+          $curl -OL $url
+        fi
+        popd
+
+      fi
+
+    done
+
+    popd
+    rm -rf $tmp
+    ;;
+
+  # HELP ----------------------------------------------------------------
+
+  all)
+    $me repos
+    $me other
+    echo
+    echo "Update OK"
+    ;;
+
+  *)
+    set +x
+    echo
+    echo "Usage: $0 <section> [<filter>]"
+    echo "...where section is one of:"
+    grep -E '\w\)$' $me | sed -e 's/)//'
+    echo
+    echo "<filter> can be used with the 'repos' section."
+    exit 1
+
+esac
+
