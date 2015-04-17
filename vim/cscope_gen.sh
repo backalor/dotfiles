eddf47b293492ac5a87392595da54892ec3059e7
backalor <backalor@backalor.com>
2015-04-17 15:16:26 +0900

    - make cscope_gen.sh work with symlinks - add fix for colors in vimrc when using putty - change case of scripts directory in path

--- vim/cscope_gen.sh
+++ vim/cscope_gen.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-find $PWD -name '*.py' \
+PWD=`pwd`
+
+find ${PWD}/ -name '*.py' \
 -o -name '*.java' \
 -o -iname '*.[CH]' \
 -o -name '*.cpp' \
 -o -name '*.cc' \
 -o -name '*.hpp'  \
-> cscope.files
+> cscope.files 2>/dev/null
 
 # -b: just build the cross-reference only
 # -q: create inverted index
 # -R: recursive
 # -i: include only these files
-cscope -b -q -R -i cscope.files
+cscope -b -q -R -i cscope.files 2>/dev/null
 
 rm cscope.files
